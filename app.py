86ab2b4a6e76b8652337236919de0f69c23b949a
Freddy Heppell <freddy@freddyheppell.com>
2020-11-07 18:04:29 +0000

    Update authorise method

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from os import environ
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 import spotify
 import boto3
 import share_code
 
 app = Flask(__name__)
+CORS(app)
 
 USERS_TABLE = environ.get('USERS_TABLE')
 IS_OFFLINE = environ.get('IS_OFFLINE')
@@ -36,32 +38,45 @@ def profile():
 @app.route("/authorise", methods=["POST"])
 def authorise():
     json = request.get_json()
+    spotify_code = json["spotify_code"]
     passthrough_share_code = json["share_code"] if "share_code" in json else None
 
+    access_token = spotify.exchange_code(spotify_code)
+
     data = {
-        "access_token": json["access_token"],
-        "expires_in": json["expires_in"],
-        "refresh_token": json["refresh_token"]
+        "access_token": access_token["access_token"],
+        "expires_in": access_token["expires_in"],
+        "refresh_token": access_token["refresh_token"]
     }
 
     # Get the user's ID
     user_id = spotify.get_user_from_token(data["access_token"])["id"]
-    data["user_id"] = user_id
-
-    # Make a share code for this user
-    data["share_code"] = share_code.generate_share_code(word_list)
 
     # Persist to db
     resp = client.put_item(
         TableName=USERS_TABLE,
         Item= {
-            "share_code": {'S': data["share_code"]},
-            "access_token": {'S': data["access_token"]},
-            "refresh_token": {'S': data["refresh_token"]},
-            "expires_in": {'S': data["expires_in"]},
+            "share_code": {'S': share_code.generate_share_code(word_list)},
+            "access_token": {'S': access_token["access_token"]},
+            "refresh_token": {'S': access_token["refresh_token"]},
+            "expires_in": {'S': access_token["expires_in"]},
+            "user_id": {'S': user_id},
         }
     )
 
     return {
         "share_codes": [data["share_code"], passthrough_share_code]
     }
+
+# @app.route("/compare", methods=["POST"])
+# def compare():
+#     json = request.get_json()
+#     share_codes = json["share_codes"]
+
+#     if len(share_codes) != 2:
+#         return jsonify({"error": "Must provide 2 codes"}), 422
+
+#     resp = {}
+
+#     for share_code in share_codes:
+        
--- spotify.py
+++ spotify.py
@@ -1,4 +1,6 @@
 import requests
+from os import environ
+from base64 import b64encode
 
 def sanitise_profile_response(spotify_response):
     response = {
@@ -17,3 +19,28 @@ def get_user_from_token(token):
         raise Exception(resp.json())
 
     return resp.json()
+
+def exchange_code(code):
+    body = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": environ.get('REDIRECT_URI')
+    }
+
+    headers = {
+        "Authorization": basic_encoded()
+    }
+
+    resp = requests.post('https://accounts.spotify.com/api/token', data=body, headers=headers)
+
+    if resp.status_code != 200:
+        raise Exception(resp.json())
+
+    return resp.json()
+
+def basic_encoded():
+    key_string = environ.get('CLIENT_ID')+ ":" + environ.get('CLIENT_SECRET')
+    key_string_encoded = key_string.encode('ascii')
+    key_string_b64 = b64encode(key_string_encoded)
+
+    return "Basic " +  key_string_b64.decode('ascii')
