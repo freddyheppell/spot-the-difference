cdc8cf77072eac64e347555abc3a9466e443b9ad
Freddy Heppell <freddy@freddyheppell.com>
2020-11-07 13:12:45 +0000

    Fix some warnings

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/hello/<string:name>")
 
-def hello():
+def hello(name):
 
-    return "Hello World!"
+    return "Hello" + name + "!"
