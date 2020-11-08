5d0aefa5b9d167a3c50d3dcf72fc14978e7990a2
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-11-08 03:55:22 +0000

    Make term selector scale better on smaller screen widths

--- client/src/components/TermSelector.vue
+++ client/src/components/TermSelector.vue
@@ -2,17 +2,20 @@
   <ol class="terms-ol">
     <li :class="{ 'term-li':true, 'selected':selected_term=='long_term' }" 
         @click="selectTerm('long_term')">
-        Long Term
+        Long
+        Term
         <hr v-if="selected_term=='long_term'"/>
     </li>
     <li :class="{ 'term-li':true, 'selected':selected_term=='medium_term' }" 
         @click="selectTerm('medium_term')">
-        Medium Term
+        Medium
+        Term
         <hr v-if="selected_term=='medium_term'"/>
     </li>
     <li :class="{ 'term-li':true, 'selected':selected_term=='short_term' }" 
         @click="selectTerm('short_term')">
-        Short Term
+        Short
+        Term
         <hr v-if="selected_term=='short_term'"/>
     </li>
   </ol>
@@ -37,13 +40,19 @@ export default {
   display:flex;
   width:100%;
   flex-wrap: nowrap;
+  overflow-x:auto;
   .term-li {
     flex-grow:1;
+    padding: 0 $spacer*2;
     color: $magenta-l;
     text-shadow: 0px 0px 15px $magenta-d;
     &.selected {
       color: $cyan-l;
       text-shadow: 0px 0px 15px $cyan-d;
+    }
+    white-space:pre-line;
+    @media(min-width:$breakpoint-width) {
+      white-space: normal;
     }
   }
 }
