163b3eb66e58b0f5f91013d58e3593feed1aa58b
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-11-09 22:56:51 +0000

    Change link icon

--- client/src/plugins/fontawesome.js
+++ client/src/plugins/fontawesome.js
@@ -3,10 +3,10 @@ import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 library.add(
-  faExternalLinkAlt,
+  faArrowUp,
 )
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
