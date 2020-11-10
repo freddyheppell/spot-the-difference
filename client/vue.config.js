814f5c1d0dc5412f092c9ca13ddf1e8f13ff2faa
Joshua O'Sullivan <jrosullivan1@sheffield.ac.uk>
2020-11-10 18:48:21 +0000

    Create _fonts.scss

--- client/vue.config.js
+++ client/vue.config.js
@@ -7,6 +7,7 @@ module.exports = {
         @import "~@/assets/styles/reset.css";
         @import "~@/assets/styles/_vars.scss";
         @import "~@/assets/styles/_mixins.scss";
+        @import "~@/assets/styles/_fonts.scss";
         @import "~@/assets/styles/style.scss";        
         `
       }
